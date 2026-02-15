5b010fa361525ae66c6698b89040d876ba7fb0ba
Michael Rolf Onyango <rolfonyango@gmail.com>
2026-02-16 01:43:26 +0300

    Add Kenya Shillings formatting styles

--- script.js
+++ script.js
@@ -8,4 +8,19 @@ const STORAGE_KEY = "smart budget transactions"
 // LOCAL STORAGE FUNCTIONS
 // =============================
 
+function getTransactions () {
+    return JSON.parse(localStorage.getItem(STORAGE_KEY)) || [];
+}
+
+function saveTransactions(transactions) {
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(transactions))
+}
+
+function formatCurrency(amount) {
+    return "Ksh" + amount.toLocaleString ("en-KE", {
+        minimumFractionDigits: 2
+    });
+}
+
+
 
